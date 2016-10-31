9d37d89dbcaad9b51252cc6f9cff6b03d0068dcb
hannako <jessjones1@gmail.com>
2016-10-31 21:37:42 +0000

    refactors using remote: true in teh form for to get rid of todos.js Rails MAGIC

--- app/assets/javascripts/todos.js
+++ app/assets/javascripts/todos.js
@@ -1,20 +1,20 @@
-$(function(){
-  $("form").submit(function(){
-    event.preventDefault();
-    var action = $(this).attr('action');
-    var method = $(this).attr('method');
-    var description = $(this).find('#todo_description').val();
-    var priority = $(this).find('#todo_priority').val();
-    var data = $(this).serializeArray();
-    //the .serializeArray method formats the params for Rails. SO if you get error
-    //message ActionController::ParameterMissing (param is missing or the value is empty: todo):
-    //this is the solution!
-
-    $.ajax({
-      method: method,
-      url: action,
-      data: data,
-      dataType: 'script'
-    });
-  });
-});
+// $(function(){
+//   $("form").submit(function(){
+//     event.preventDefault();
+//     var action = $(this).attr('action');
+//     var method = $(this).attr('method');
+//     var description = $(this).find('#todo_description').val();
+//     var priority = $(this).find('#todo_priority').val();
+//     var data = $(this).serializeArray();
+//     //the .serializeArray method formats the params for Rails. SO if you get error
+//     //message ActionController::ParameterMissing (param is missing or the value is empty: todo):
+//     //this is the solution!
+//
+//     $.ajax({
+//       method: method,
+//       url: action,
+//       data: data,
+//       dataType: 'script'
+//     });
+//   });
+// });
